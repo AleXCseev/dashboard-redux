03e009bb0536acfc57e6907d2adda7d1660fe5d9
Kontantin Alekseev <alexcseev1990@gmail.com>
2022-04-21 14:32:00 +0300

    RTK

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux"
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
-import { store, persistor } from "./store"
-import { PersistGate } from 'redux-persist/integration/react'
 
+import {store} from './store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
